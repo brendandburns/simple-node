89709c98833ae1dcb84b3d24d1487958fd7a7718
Brendan Burns <bburns@marvel.home>
2016-11-02 22:12:53 -0700

    updated.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // Load the http module to create an http server.
 
 var http = require('http');
-
-
+var os = require('os');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
@@ -15,4 +14,4 @@ var server = http.createServer(function (request, response) {
 server.listen(8000);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running at http://" + os.hostname() + ":8000/");
