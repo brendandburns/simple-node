abc6e506d721bceee12a3a8f5e475479dc8249f6
Brendan Burns <bburns@marvel.home>
2016-11-05 11:50:46 -0700

    updated app.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var os = require('os');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.end("<body>" +
-              "Hello World this is a really cool test for Brendan and KubeCon and YouTube!" +
+              "Hello World this is a really cool test for Brendan." +
                "<br>Served by: \n" + os.hostname() + 
                "</body>");
   console.log("Request Handled: " + request.url);
