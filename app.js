7a1f74c90cb482e1a5835b2a52c3cdcde3889fb0
Brendan Burns <bburns@marvel.home>
2016-11-05 19:57:39 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// Load the http module to create an http server.
-
 var http = require('http');
 var os = require('os');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
+var srv = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.end("<body>" +
               "Hello World this is a really cool test for Brendan." +
@@ -13,8 +10,5 @@ var server = http.createServer(function (request, response) {
   console.log("Request Handled: " + request.url);
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
-
-// Put a friendly message on the terminal
 console.log("Server running at http://" + os.hostname() + ":8000/");
